91ccbd2faff0c26b484c5bb1452e2bee9681699d
Mohammed ARBAOUI <blood_prison@hotmail.fr>
2018-07-20 04:15:08 +0000

    [route] pas fini Edit

--- cfa-beta/config/routes.rb
+++ cfa-beta/config/routes.rb
@@ -7,11 +7,12 @@ Rails.application.routes.draw do
 
   post 'users/login' => 'users#check'
   delete 'users/login' => 'users#logout'
-  get 'users/:user_name/edit' => 'users#edit' as: 'users/edit/sitting'
-  get 'users/register'
 
+  get 'users/register'
   post 'users/register' => 'users#create'
 
+  get 'users/:user_name/edit' => 'users#edit' as: 'users/edit/sitting'
+  put 'users/:user_name/edit' => 'users#update'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
